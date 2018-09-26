e1ffe5f935376ee64900e6ed6217455c92f0d42c
Jackie Pearce <jackiepearce83@hotmail.com>
2018-09-26 11:34:15 +0100

    solved solveSquare

--- SudokuSolver2/SudokuSolver2/SolveSquare.cs
+++ SudokuSolver2/SudokuSolver2/SolveSquare.cs
@@ -10,7 +10,45 @@ namespace SudokuSolver2
     {
         public void Solve(Board board)
         {
+            for (int X = 0; X < 3; X++)
+            {
+                for (int Y = 0; Y < 3; Y++)
+                {
+                    SolveSquareXY(board, X, Y);
+                }
 
+            }
+            
+        }
+
+       
+        private static void SolveSquareXY(Board board, int X,int Y)
+        {
+            var currentSquareMissing = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int NumberOfZeros = 0;
+            int whereIsTheZeroY = -1;
+            int whereIsTheZeroX = -1;
+            for (int x = 3*X; x < (3*X + 3); x++)
+            {
+                for (int y = 3*Y; y < (3*Y + 3); y++)
+                {
+                    var currentSquare = board.BoardType[x][y];
+                    if (currentSquare > 0)
+                    {
+                        currentSquareMissing.Remove(currentSquare);
+                    }
+                    if (currentSquare == 0)
+                    {
+                        whereIsTheZeroY = y;
+                        whereIsTheZeroX = x;
+                        NumberOfZeros++;
+                    }
+                }
+            }
+            if (NumberOfZeros == 1)
+            {
+                board.BoardType[whereIsTheZeroX][whereIsTheZeroY] = currentSquareMissing[0];
+            }
         }
     }
 }
