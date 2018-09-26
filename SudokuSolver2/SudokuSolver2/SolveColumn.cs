f52dcb9565125ea2eac6ec00e594e391a00ff7ef
Jackie Pearce <jackiepearce83@hotmail.com>
2018-09-26 10:08:35 +0100

    Wrote solve method in solveColumn

--- SudokuSolver2/SudokuSolver2/SolveColumn.cs
+++ SudokuSolver2/SudokuSolver2/SolveColumn.cs
@@ -10,11 +10,27 @@ namespace SudokuSolver2
     {
         public void Solve(Board board)
         {
-            for (int x = 0; x < 9; x++)
+            for (int y = 0; y < 9; y++)
             {
-                for (int y = 0; y < 9; y++)
+                var currentColumnMissing = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                int NumberOfZeros = 0;
+                int whereIsTheZero = -1;
+                for (int x = 0; x < 9; x++)
                 {
                     var currentSquare = board.BoardType[x][y];
+                    if (currentSquare > 0)
+                    {
+                        currentColumnMissing.Remove(currentSquare);
+                    }
+                    if (currentSquare == 0)
+                    {
+                        NumberOfZeros++;
+                        whereIsTheZero = x;
+                    }
+                }
+                if (NumberOfZeros == 1)
+                {
+                    board.BoardType[whereIsTheZero][y] = currentColumnMissing[0];
                 }
             }
         }
