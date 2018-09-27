ab86ee04f9f7c6160040070f7f362ac64239f8e4
Jackie Pearce <jackiepearce83@hotmail.com>
2018-09-27 11:46:35 +0100

    Write SuggestedValues removal to SolveSquare

--- SudokuSolver2/SudokuSolver2/SolveBox.cs
+++ SudokuSolver2/SudokuSolver2/SolveBox.cs
@@ -18,38 +18,55 @@ namespace SudokuSolver2
                 }
 
             }
-            
+
         }
 
-       
-        private static void SolveBoxXY(Board board, int X,int Y)
+
+        private static void SolveBoxXY(Board board, int X, int Y)
         {
             var currentSquareMissing = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int NumberOfZeros = 0;
-            int whereIsTheZeroY = -1;
-            int whereIsTheZeroX = -1;
-            for (int x = 3*X; x < (3*X + 3); x++)
+            var currentSquareUsed = new List<int> { };
+            //int NumberOfZeros = 0;
+            //int whereIsTheZeroY = -1;
+            //int whereIsTheZeroX = -1;
+            for (int x = 3 * X; x < (3 * X + 3); x++)
             {
-                for (int y = 3*Y; y < (3*Y + 3); y++)
+                for (int y = 3 * Y; y < (3 * Y + 3); y++)
                 {
                     var currentSquare = board.BoardState[x][y].ConfirmedValue;
                     if (currentSquare > 0)
                     {
                         currentSquareMissing.Remove(currentSquare);
+                        currentSquareUsed.Add(currentSquare);
                     }
-                    if (currentSquare == 0)
+                    //if (currentSquare == 0)
+                    //{
+                    //    whereIsTheZeroY = y;
+                    //    whereIsTheZeroX = x;
+                    //    NumberOfZeros++;
+                    //}
+                }
+                for (int y = 0; y < 9; y++)
+                {
+                    var currentSquare = board.BoardState[x][y];
+                    if (currentSquare.ConfirmedValue == 0)
                     {
-                        whereIsTheZeroY = y;
-                        whereIsTheZeroX = x;
-                        NumberOfZeros++;
+
+                        foreach (int value in currentSquareUsed)
+                        {
+                            currentSquare.SuggestedValues.Remove(value);
+                        }
                     }
+
                 }
             }
-            if (NumberOfZeros == 1)
-            {
-                var square = board.BoardState[whereIsTheZeroX][whereIsTheZeroY];
-                square.ConfirmedValue = currentSquareMissing[0];
-            }
+            // Remove suggestedValues from zero value boardSquares
+
+            //if (NumberOfZeros == 1)
+            //{
+            //    var square = board.BoardState[whereIsTheZeroX][whereIsTheZeroY];
+            //    square.ConfirmedValue = currentSquareMissing[0];
+            //}
         }
     }
 }
